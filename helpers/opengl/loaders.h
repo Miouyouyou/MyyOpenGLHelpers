28a6be1e192e6a7bc6180bf227b0bd07e1a6f0d3
Myy <myy@miouyouyou.fr>
2017-03-04 18:55:26 +0000

    Documented the new OpenGL shaders loaders helpers.
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- helpers/opengl/loaders.h
+++ helpers/opengl/loaders.h
@@ -9,22 +9,128 @@ int glhLoadShader
 (GLenum const shaderType, char const * __restrict const name,
  GLuint const program);
 
+/**
+ * Compiles a GLSL program, using informations from the provided
+ * metadata, notably metadata->shaders and metadata->strings.
+ * These metadata are generated automatically by another software.
+ * 
+ * @params
+ * @param metadata A user-generated structure defining the
+ *                 referenced shaders filepaths.
+ * @param n_shaders The number of shaders composing the program
+ * @param shaders A pointer to a list of user-generated shaders
+ *                references to be loaded. The shaders metadata
+ *                are provided by the first argument.
+ * 
+ * @returns The successfully program ID
+ * @returns 0 if something went wrong during the program
+ *          compilation
+ * 
+ * @example
+ * enum glsl_shader_name {
+ * 	text_vsh,
+ * 	text_fsh,
+ * 	glsl_shaders_count
+ * };
+ * 
+ * struct glsl_programs_shared_data {
+ * 	GLuint programs[glsl_programs_count];
+ * 	struct glsl_shader shaders[glsl_shaders_count];
+ * 	GLchar strings[512];
+ * } glsl_shared_data = {
+ * 	.programs = {0},
+ * 	.strings = {
+ * 	  "shaders/standard.vert\0shaders/standard.frag\0"
+ * 	},
+ * 	.shaders = {
+ * 	  [text_vsh] = {
+ *	    .type = GL_VERTEX_SHADER,
+ *	    .str_pos = 0,
+ *	  },
+ *	  [text_fsh] = {
+ *	    .type = GL_FRAGMENT_SHADER,
+ *	    .str_pos = 22,
+ *	  }
+ *	}
+ * };
+ * 
+ * GLuint loaded_shaders[] = {text_vsh, text_fsh};
+ * 
+ * glhCompileProgram(&glsl_shared_data, 2, loaded_shaders);
+ * 
+ */
 unsigned int glhCompileProgram
 (struct glsl_programs_shared_data const * __restrict const metadata,
  unsigned int const n_shaders,
  enum glsl_shader_name const * __restrict const shaders);
 
+/**
+ * Link and Save a precompiled program.
+ * The program is saved in metadata->programs[program_index]
+ * 
+ * @params
+ * @param metadata A user-generated structure containing the address
+ *                 where to save the linked program reference.
+ * @param program_index A user-generated and defined program reference
+ *                      index.
+ * @param p The GLSL program to link
+ * 
+ * @returns The saved GLSL reference if linking performed successfully
+ * @returns 0 if linking the GLSL program failed.
+ */
 unsigned int glhLinkAndSaveProgram
 (struct glsl_programs_shared_data * __restrict const metadata,
  enum glsl_program_name const program_index,
  GLuint const p);
 
+/**
+ * Compile, Link and Save a GLSL program.
+ * This combines glhCompileProgram and glhLinkAndSaveProgram.
+ * 
+ * @params
+ * @param metadata A user-generated structure defining the
+ *                 referenced shaders filepaths and the address where
+ *                 to save the linked program.
+ * @param n_shaders The number of shaders composing the program
+ * @param shaders A pointer to a list of user-generated shaders
+ *                references to be loaded. The shaders metadata
+ *                are provided by the first argument.
+ * @param program_index A user-generated and defined program reference
+ *                      index.
+ * 
+ * @returns the saved GLSL program ID if everything went fine.
+ * @returns 0 if something went wrong during the compilation or linking
+ *          phase.
+ */
 unsigned int glhBuildAndSaveProgram
 (struct glsl_programs_shared_data * __restrict const metadata,
  unsigned int const n_shaders,
  enum glsl_shader_name const * __restrict const shaders,
  enum glsl_program_name const program_index);
 
+/**
+ * Compile, Link and Save a GLSL program.
+ * Instead of requiring an array of shaders references, and the array
+ * length, this function takes two shaders references that are expected
+ * to be Vertex Shader reference and a Fragment Shader reference.
+ * 
+ * This is function covers the basic form of GLSL programs.
+ * 
+ * @params
+ * @param metadata A user-generated structure defining the
+ *                 referenced shaders filepaths and the address where
+ *                 to save the linked program.
+ * @param vertex_shader A user-generated Vertex Shader reference to be
+ *                      used in the GLSL program to build.
+ * @param fragment_shader A user-generated Fragment Shader reference to
+ *                        be used in the GLSL program to build.
+ * @param program_index A user-generated and defined program reference
+ *                      index.
+ * 
+ * @returns The saved GLSL program ID if everthing went fine.
+ * @returns 0 if something went wrong during the compilation or linking
+ *          phase.
+ */
 unsigned int glhBuildAndSaveSimpleProgram
 (struct glsl_programs_shared_data * __restrict const metadata,
  enum glsl_shader_name vertex_shader,
